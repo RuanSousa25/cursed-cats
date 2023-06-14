541a7d6665f27c84cec35f9847ae7fd6a1289e48
Ruan Sousa <103215747+RuanSousa25@users.noreply.github.com>
2023-06-14 14:37:36 -0300

    Add files via upload

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,12 @@
-import styles from "./Home.module.css"
-import { useEffect } from "react";
+import styles from "./Home.module.css";
+import { useEffect, useState } from "react";
 import { useFetchBucket } from "../../Hooks/useFetchBucket";
+
+
+
 export default function Home(){
     const {files} = useFetchBucket();
+    const [hoveredImage, setHoveredImage] = useState(null);
 
 
     return(
@@ -11,10 +15,13 @@ export default function Home(){
         <div className={styles.main}>
         {files && files.length !== 0 &&
             files.map(
-                (e)=>
-                <img src={e} className={
-                    Math.floor(Math.random() * 5) + 1
-                }></img>
+                (e, index)=>
+                <img src={e} 
+                key={index}
+                className={styles["image"+(Math.floor(Math.random() * 5) + 1)]}
+
+
+                ></img>
                    
                 
             )
