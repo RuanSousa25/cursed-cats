c62dd2b13a5820c625456c7a3558801efb0d3ddb
Ruan Sousa <103215747+RuanSousa25@users.noreply.github.com>
2023-06-23 14:37:29 -0300

    Add files via upload

--- src/Pages/Post/Post.js
+++ src/Pages/Post/Post.js
@@ -1,23 +1,34 @@
 import { Link } from "react-router-dom";
 import styles from "./Post.module.css";
-export default function Post(){
-    
+import $ from "jquery";
+import { useState } from "react";
+import { useRef } from "react";
+export default function Post() {
+    const [input, setInput] = useState();
+    const inputRef = useRef();
+    function enviarDados(e) {
+        e.preventDefault();
+        console.log(inputRef.current.files[0]);
+    }
+
+    console.log(inputRef)
+
     return (
-         <>
-    <h1>Postar</h1>
-      
-        <form className={styles.form}>
-            <label>Nome do gato: </label>
-            <input type="text"></input>
-            <label className={styles.file_custom_empty}>
-                Imagem:
-            <input type="file" name="arquivo"></input>
+        <>
+            <h1>Postar</h1>
+
+            <form className={styles.form}>
+                <label>Nome do gato: </label>
+                <input type="text"></input>
+                <label className={inputRef.current?.files.lenght ? styles.file_custom_empty : styles.file_custom_filled}>
+                    Imagem:
+                    <input type="file" ref={inputRef} name="arquivo" id="input_file" ></input>
                 </label>
 
-                <button>Enviar</button>
-            
-            
-        </form>
-    </>
+                <button onClick={enviarDados}>Enviar</button>
+
+
+            </form>
+        </>
     )
 }
