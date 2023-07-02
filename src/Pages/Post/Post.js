308a067f618fbdce4f319cb7cd670759866b9bf9
Ruan Sousa <103215747+RuanSousa25@users.noreply.github.com>
2023-07-02 09:23:43 -0300

    Add files via upload

--- src/Pages/Post/Post.js
+++ src/Pages/Post/Post.js
@@ -1,17 +1,36 @@
 import { Link } from "react-router-dom";
 import styles from "./Post.module.css";
 import $ from "jquery";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRef } from "react";
 export default function Post() {
     const [input, setInput] = useState();
     const inputRef = useRef();
-    function enviarDados(e) {
-        e.preventDefault();
-        console.log(inputRef.current.files[0]);
-    }
+    const [imgUrl, setImgUrl] = useState("");
 
-    console.log(inputRef)
+  useEffect(() => {
+    inputRef.current.addEventListener("change", handleFileChange);
+  }, []);
+
+  const handleFileChange = () => {
+    let file = inputRef.current.files[0];
+    setInput(inputRef.current.files[0]);
+
+    if(file && file.type.startsWith("image/")){
+        let imgUrlTemp = URL.createObjectURL(file);
+        setImgUrl(imgUrlTemp);
+        console.log(imgUrl);
+    }else return
+  };
+
+  const isFileSelected = inputRef.current?.files.length > 0;
+  const fileLabelClassName = isFileSelected ? styles.file_custom_filled : styles.file_custom_empty;
+
+  function enviarDados(e) {
+    e.preventDefault();
+    console.log(input);
+  }
+
 
     return (
         <>
@@ -20,9 +39,10 @@ export default function Post() {
             <form className={styles.form}>
                 <label>Nome do gato: </label>
                 <input type="text"></input>
-                <label className={inputRef.current?.files.lenght ? styles.file_custom_empty : styles.file_custom_filled}>
-                    Imagem:
+                <label className={fileLabelClassName}>
+                    Imagem
                     <input type="file" ref={inputRef} name="arquivo" id="input_file" ></input>
+                    {imgUrl && <img src={imgUrl} alt="imagem selecionada"/>}
                 </label>
 
                 <button onClick={enviarDados}>Enviar</button>
